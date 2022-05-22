06ada076e9d11e49cae5b0094fbbcbf37884029e
kachDev7 <99486031+kachDev7@users.noreply.github.com>
2022-05-22 23:36:14 +0100

    my commit

--- pages/wallet/loading.js
+++ pages/wallet/loading.js
@@ -1,7 +1,9 @@
 const loading = () => {
     return(
-        <div className="d-flex jac container">
-            <div className="loader"></div>
+        <div className="d-flex jac coll container err-holder my-3">
+            <div className="text-danger fw-bold"><i class="bi-x-octagon-fill h1 err"></i></div>
+            <div className="lead text-center"><p>Sorry, Something went wrong. Temporary error!</p></div>
+            <div className="text-center"><button className="btn btn-primary">Go Back</button></div>
         </div>
     )
 }
